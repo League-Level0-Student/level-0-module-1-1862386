0065699e72d9866094426f697758d1ccb58f282d
League <League@AdminisorsiMac2.attlocal.net>
2018-06-07 19:58:47 -0700

    Remarkable and Badgers

--- src/Badger.java
+++ src/Badger.java
@@ -1,8 +1,28 @@
 
 public class Badger {
-public static void main(String[] args) {
-	for (int i=0; i <10; i++) {
-		System.out.println("badger");
-	}
+	public static void main(String[] args) {
+		for (int r = 0; r < 5; r++) {
+
+			for (int i = 0; i < 10; i++) {
+				System.out.println("badger");
+
+			}
+
+			if (r == 3) {
+				for (int h = 0; h < 2; h++)
+					
+				{
+					System.out.println("Snake");
+				}
+			}
+
+			if (r != 3) {
+				for (int k = 0; k < 2; k++) {
+
+					System.out.println("Mushroom");
+				}
+			}
+
+		}
 	}
 }
---
+++ src/Remarkable.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+String Roshan = "Always hard working";		
+String bottle = JOptionPane.showInputDialog("Who do you want to here about");
+
+	
+}
+}
