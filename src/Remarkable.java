a2be7ab4e867a759844b67cff002ed419e3618af
1862386 <league@WTS-IM-17.attlocal.net>
2018-06-14 19:58:37 -0700

    Tomato and Remarkable

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -2,9 +2,25 @@ import javax.swing.JOptionPane;
 
 public class Remarkable {
 	public static void main(String[] args) {
-String Roshan = "Always hard working";		
-String bottle = JOptionPane.showInputDialog("Who do you want to here about");
-
-	
-}
+		String bottle = JOptionPane.showInputDialog("Who do you want to here about");
+		if (bottle.equals("Roshan")) {
+			JOptionPane.showMessageDialog(null, "He is always hardworking");
+		}
+ if (bottle.equals("Milan")) {
+	JOptionPane.showMessageDialog(null, "He is always making me laugh and an amazing brother!");
+ }		
+	if (bottle.equals("Kai")) {
+		JOptionPane.showMessageDialog(null, "He is a great tennis player and is extremly funny");
+	}		
+	if (bottle.equals("Liam")) {	
+		JOptionPane.showMessageDialog(null, "He is very helpful");
+	}		
+	if(bottle.equals("Michael"))	{
+		JOptionPane.showMessageDialog(null, "Is always caring and helpful");
+	}		
+		
+		
+		
+		
+	}
 }
